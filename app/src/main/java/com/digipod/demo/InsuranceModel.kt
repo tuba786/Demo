4ab583633c915cee7d946766205703b163208583
Shafaq Fatima <shafaq93fatima@gmail.com>
2023-05-30 18:54:18 +0530

    almost finished Alhamdulilah

---
+++ app/src/main/java/com/digipod/demo/InsuranceModel.kt
@@ -0,0 +1,5 @@
+package com.digipod.demo
+
+data class InsuranceModel(val name: String = "",
+                          val description: String = "",
+                          val price: String = "100")
